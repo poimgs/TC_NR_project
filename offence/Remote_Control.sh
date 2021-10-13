2603979917b9e00c294ba22ad59ad1816768223f
poimgs <stevenchia56@gmail.com>
2021-10-13 07:03:03 -0400

    Complete Remote_Control.sh (offence)

--- offence/Remote_Control.sh
+++ offence/Remote_Control.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
-# Configuration variables
-
-# Uncomment the following line and enter your country (ISO format)
-# origin_country=
-
-# Uncomment the following line and enter your VPS details
-# server=
-# user=
-# password=
+# =====================
+# Functions begins here
+# =====================
 
 function help_message() {
 	echo "Run commands anonymously: two layer anonymity using Tor and VPS"
 	echo "==============================================================="
 	echo
-	echo "Syntax: Remote_Control.sh [-f ips_file | ip_address] "
+	echo "Syntax: Remote_Control.sh -c ISO_Country [-f ips_file] | ip_address "
 	echo "options:"
+	echo "	c: Country in ISO format"
 	echo "	f: [Optional] Run Remote Control on a file of ips"
 	echo "	h: Help prompt"
 	echo
-	echo "Note: This script assumes that you have the commands nmap and whois on your VPS."
-	echo "Please install these commands if they are not available on your vps"
+	echo "Set-up: Input your VPS login information in Remote_Control.conf"
 	
 }
 
@@ -50,6 +44,14 @@ function inst() {
 		sudo apt-get -y install sshpass
 	}
 	
+	createconf() {
+		echo "[*] Creating Remote_Control.conf file"
+		echo "# VPS Details" > Remote_Control.conf 
+		echo "server=" > Remote_Control.conf
+		echo "user=" > Remote_Control.conf
+		echo "password=" > Remote_Control.conf
+	}
+	
 	# Check for nipe
 	if ! [ -d "/usr/local/src/nipe" ]; then
 		installnipe
@@ -64,16 +66,22 @@ function inst() {
 	if ! [ -x "$(command -v sshpass)" ]; then
 		installsshpass
 	fi
+	
+	# Check for Remote_Control.conf file
+	if ! [ -f ./Remote_Control.conf ]; then
+		createconf
+	fi
 }
 
 function anon() {
 	# Get Current ip address
+	country=$1
 	cd /usr/local/src/nipe
 	current_IP=$(sudo perl nipe.pl status | grep Ip | awk '{print $NF}')
 	current_loc=$(geoiplookup $current_IP | awk '{print $4}' | sed 's/,//')
 	
 	# Check country of ip address
-	if [ "$current_loc" == "$origin_country" ]; then
+	if [ "$current_loc" == "$country" ]; then
 		# Run nipe if current location is Singapore
 		echo "[!] You are not anonymous"
 		echo "[*] Running Nipe"
@@ -120,7 +128,7 @@ function vps() {
 	fi
 	
 	if [ "$nmap_query" == "yes" ]; then
-		nmap -sV -Pn -p-  --host-timeout 10m $ip -oN nmap_result 
+		nmap -sV -Pn -p- --host-timeout 10m $ip -oN nmap_result 
 	fi
 }
 
@@ -131,45 +139,93 @@ function vps() {
 # Check if user has packages needed to run command
 inst
 
-# Check if user has inputted configuration variables
-if [ -z "$origin_country" ] || [ -z "$server" ] || [ -z "$user" ] || [ -z "$password" ]; then
-	echo "[!] Please enter your configuration details at the top of the script before utilising the script"
-	exit 1
-fi
-
 # Check if user has inputted any argument
 if [ $# -eq 0 ]; then
 	echo "[!] You did not provide any arguments"
+	echo
 	help_message
 	exit 1
 fi
 
 # Parse user input
-while getopts ":hs:f:" opt; do
+while getopts ":hc:f:" opt; do
 	case ${opt} in
 		h)
 			help_message
 			exit
 		;;
-		s)
-			script=$OPTARG
+		c)
+			country=$OPTARG
 		;;
 		f)
 			ips=$OPTARG
 		;;
 		\?)
 			echo "[!] Invalid option: $OPTARG" 1>&2
+			echo
 			help_message
 			exit 1
 		;;
 		:)
 			echo "[!] Invalid option: $OPTARG requires an argument" 1>&2
+			echo
 			help_message
 			exit 1
 		;;
 	esac
 done
 shift $((OPTIND -1))
+
+# Get ip address if user inputted one
+ip=$1
+
+# Check if user has sepcified an ISO country
+if [ -z "$country" ]; then
+	echo "[!] Please specify a country with the -c flag."
+	echo
+	help_message
+	exit 1
+fi
+
+# Check if user specified both a list of ips and an ip
+if [ -e "$ips" ] && [ -n "$ip" ]; then
+	echo "[!] You have entered both a file of ips and an ip. Please only specify one."
+	echo
+	help_message
+	exit 1
+fi 
+
+# Get server login information from Remote_Control.conf
+while read -r line; do
+	# Skip comment lines
+	if [ "${line::1}" == "#" ]; then
+		continue
+	fi
+	
+	# Get configuration variables
+	var=$(echo $line | awk -F= '{print $1}')
+	value=$(echo $line | awk -F= '{print $2}')
+	
+	# If any of the fields are empty, exit script
+	if [ -z "$value" ]; then
+		echo "[!] There is no value specified for $var"
+		echo "[*] Please enter your VPS login information in the Remote_Control.conf file"
+		exit 1
+	fi
+	
+	# Get values from conf file
+	if [ "$var" == "server" ]; then
+		server="$value"
+	fi
+	
+	if [ "$var" == "user" ]; then
+		user="$value"
+	fi
+	
+	if [ "$var" == "password" ]; then
+		password="$value"
+	fi
+done < <(cat Remote_Control.conf | sed '1d')
 
 # Get user input on what services to run on specified ip(s)
 
@@ -221,21 +277,20 @@ esac
 date=$(date +%d%m%y)
 
 # Start anonymity service
-anon
+anon $country
 
 # Check if user inputted file of ips
 if [ -e "$ips" ]; then
 	for ip in $(cat $ips); do
 		echo "[*] Running scripts on $ip"
 		working_dir=./Remote_Control_logs/$ip/$date
-		sshpass -p $password ssh $user@$server "$(typeset -f vps); vps $ip $working_dir $whois_query $nmap_query"
+		sshpass -p $password ssh -o StrictHostKeyChecking=no $user@$server "$(typeset -f vps); vps $ip $working_dir $whois_query $nmap_query"
 	done
 # Run scripts for given ip
 else
-	ip=$1
+	echo "[*] Running scripts on $ip"
 	working_dir=./Remote_Control_logs/$ip/$date
-	sshpass -p $password ssh $user@$server "$(typeset -f vps); vps $ip $working_dir $whois_query $nmap_query"
-	echo $?
+	sshpass -p $password ssh -o StrictHostKeyChecking=no $user@$server "$(typeset -f vps); vps $ip $working_dir $whois_query $nmap_query"
 fi
 
 # Stop anonymity service
